16a1a2ee5826a88ec5329324687ae985754eedbc
sswahn <36422765+sswahn@users.noreply.github.com>
2023-11-03 10:35:29 -0400

    Update ShareButton.js

--- src/core/ShareButton/ShareButton.js
+++ src/core/ShareButton/ShareButton.js
@@ -1,1 +1,32 @@
-// --
+import { useState, useEffect } from 'react'
+import { debounce } from '../../utilities/debounce'
+import ShareIcon from './ShareIcon'
+import SharedIcon from './SharedIcon'
+import styles from './styles.module.css'
+
+const ShareButton = ({ className, initialCount, onChange }) => {
+  const [shared, setShared] = useState(false)
+  const [count, setCount] = useState(0)
+  const debouncedOnChange = debounce(onChange, 500)
+
+  const handleClick = () => {
+    const isShared = !shared
+    const newCount = isShared ? count + 1 : count - 1
+    setShared(isShared)
+    setCount(newCount)
+    debouncedOnChange({ shared: isShared, count: newCount })
+  }
+
+  useEffect(() => {
+    setCount(initialCount)
+  }, [initialCount])
+
+  return (
+    <button className={`${styles.share} ${className}`} onClick={handleClick}>
+      <div>{shared ? <SharedIcon /> : <ShareIcon />} {count}</div>
+      <div className={styles.tooltip} role="tooltip">{shared ? 'Shared' : 'Share'}</div>
+    </button>
+  )
+}
+
+export default ShareButton
