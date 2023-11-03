9cc2f0cea98db4a74f6f9f71b9b166446ca3df03
sswahn <36422765+sswahn@users.noreply.github.com>
2023-11-03 10:21:36 -0400

    Update BookmarkButton.js

--- src/core/BookmarkButton/BookmarkButton.js
+++ src/core/BookmarkButton/BookmarkButton.js
@@ -2,16 +2,7 @@ import { useState } from 'react'
 import BookmarkIcon from './LikeIcon'
 import BookmarkedIcon from './LikedIcon'
 import styles from './styles.module.css'
-
-const debounce = (fn, delay) => {
-  let timeoutId
-  return (...args) => {
-    clearTimeout(timeoutId)
-    timeoutId = setTimeout(() => {
-      fn(...args)
-    }, delay)
-  }
-}
+import { debounce } from '../../utilities/debounce'
 
 const BookmarkButton = ({ className, onChange }) => {
   const [bookmark, setBookmark] = useState(false)
