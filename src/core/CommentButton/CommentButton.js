7ea781cc36ea248d5c17b604021482f1f19968c5
sswahn <36422765+sswahn@users.noreply.github.com>
2023-11-03 12:23:38 -0400

    Update and rename CommentsButton.js to CommentButton.js

--- src/core/CommentButton/CommentButton.js
+++ src/core/CommentButton/CommentButton.js
@@ -1,16 +1,12 @@
 import { useState, useEffect } from 'react'
-import { debounce } from '../../utilities/debounce'
-import CommentsIcon from './CommentsIcon'
+import CommentIcon from './CommentIcon'
 import styles from './styles.module.css'
 
-const CommentsButton = ({ className, initialCount, onChange }) => {
+const CommentButton = ({ className, initialCount, onClick }) => {
   const [count, setCount] = useState(0)
-  const debouncedOnChange = debounce(onChange, 500)
 
   const handleClick = event => {
-    const newCount = !commented ? count + 1 : count - 1
-    setCount(newCount)
-    debouncedOnChange({ count: newCount })
+    onClick()
   }
 
   useEffect(() => {
@@ -19,10 +15,10 @@ const CommentsButton = ({ className, initialCount, onChange }) => {
 
   return (
     <button className={`${styles.comments} ${className}`} onClick={handleClick}>
-      <div><CommentsIcon /> {count}</div>
+      <div><CommentIcon /> {count}</div>
       <div className={styles.tooltip} role="tooltip">Comment</div>
     </button>
   )
 }
 
-export default CommentsButton
+export default CommentButton
