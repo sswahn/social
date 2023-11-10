3ad347ca88acdaa0d404b823a5e570b8bb7d9f15
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-10 15:01:50 -0500

    Update LikeButton.js

--- src/core/LikeButton/LikeButton.js
+++ src/core/LikeButton/LikeButton.js
@@ -7,27 +7,17 @@ import styles from './styles.module.css'
 
 const LikeButton = ({ className, icon, count, onClick }) => {
   const [liked, setLiked] = useState(false)
-  //const [counter, setCounter] = useState(0)
-  const counterRef = useRef(null)
 
   const handleClick = () => {
-    const isLiked = !liked
-    //const newCount = isLiked ? counter + 1 : counter - 1
-    setLiked(isLiked)
-    //setCounter(newCount)
+    setLiked(!liked)
     onClick()
   }
-
-  useEffect(() => {
-    //setCounter(count)
-    counterRef.current.textContent = count
-  }, [count])
 
   return (
     <button className={`${styles.like} ${className || ''}`} onClick={handleClick}>
       <div>
         <div>{liked ? (icon === 'heart' ? <HeartLikedIcon /> : <HandLikedIcon />) : (icon === 'heart' ? <HeartLikeIcon /> : <HandLikeIcon />)}</div>
-        <div ref={counterRef}></div>
+        <div>{count}</div>
       </div>
       <div className={styles.tooltip} role="tooltip">{liked ? 'Liked' : 'Like'}</div>
     </button>
