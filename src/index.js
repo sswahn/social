4db8ba072e41bd85330a7d424661d8b5d0c909c7
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-10 10:35:50 -0500

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import Avatar from './core/Avatar/Avatar.js'
+import BookmarkButton from './core/BookmarkButton/BookmarkButton.js'
+import CommentButton from './core/CommentButton/CommentButton.js'
+import LikeButton from './core/LikeButton/LikeButton.js'
+import ShareButton from './core/ShareButton/ShareButton.js'
+
+
+export {
+  Avatar,
+  BookmarkButton,
+  CommentButton,
+  LikeButton,
+  ShareButton
+}
