6d73bfd182e7754da4c6b1d92106ab427e6263e9
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-10 13:43:08 -0500

    Update Avatar.js

--- src/core/Avatar/Avatar.js
+++ src/core/Avatar/Avatar.js
@@ -4,7 +4,7 @@ import styles from './styles.module.css'
 
 const Avatar = ({ className, image, username, onClick }) => {
   return (
-    <div className={`${styles.avatar} ${className}`} onClick={onClick} aria-label={`${username}'s avatar`}>
+    <div className={`${styles.avatar} ${className || ''}`} onClick={onClick} aria-label={`${username}'s avatar`}>
       {image 
         ? <img src={image} alt={`${username}'s avatar`} />
         : <UserIcon />
