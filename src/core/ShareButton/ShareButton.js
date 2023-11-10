99910cb623756676b439303b902dc79220a74344
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-10 10:19:14 -0500

    Update ShareButton.js

--- src/core/ShareButton/ShareButton.js
+++ src/core/ShareButton/ShareButton.js
@@ -1,29 +1,20 @@
-import { useState, useEffect } from 'react'
-import { debounceUtility } from '../../utilities/debounceUtility.js'
+import { useState } from 'react'
 import ShareIcon from './ShareIcon.js'
 import SharedIcon from './SharedIcon.js'
 import styles from './styles.module.css'
 
-const ShareButton = ({ className, count, onClick, debounce }) => {
+const ShareButton = ({ className, onClick, debounce }) => {
   const [shared, setShared] = useState(false)
-  const [counter, setCounter] = useState(0)
-  const debouncedOnClick = debounceUtility(onClick, debounce || 500)
 
   const handleClick = () => {
     const isShared = !shared
-    const newCount = isShared ? counter + 1 : counter - 1
     setShared(isShared)
-    setCounter(newCount)
-    debouncedOnClick({ shared: isShared, count: newCount })
+    onClick({ shared: isShared })
   }
-
-  useEffect(() => {
-    setCounter(count)
-  }, [count])
 
   return (
     <button className={`${styles.share} ${className}`} onClick={handleClick}>
-      <div>{shared ? <SharedIcon /> : <ShareIcon />} {counter}</div>
+      <div>{shared ? <SharedIcon /> : <ShareIcon />}</div>
       <div className={styles.tooltip} role="tooltip">{shared ? 'Shared' : 'Share'}</div>
     </button>
   )
