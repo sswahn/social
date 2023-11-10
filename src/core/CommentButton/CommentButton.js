b29d3ff579136a87aafdf97fbdf53a0601fccca8
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-10 15:08:24 -0500

    Update CommentButton.js

--- src/core/CommentButton/CommentButton.js
+++ src/core/CommentButton/CommentButton.js
@@ -1,23 +1,12 @@
-import { useState, useEffect } from 'react'
 import CommentIcon from './CommentIcon.js'
 import styles from './styles.module.css'
 
 const CommentButton = ({ className, count, onClick }) => {
-  const [counter, setCounter] = useState(0)
-
-  const handleClick = event => {
-    onClick()
-  }
-
-  useEffect(() => {
-    setCounter(count)
-  }, [count])
-
   return (
-    <button className={`${styles.comments} ${className || ''}`} onClick={handleClick}>
+    <button className={`${styles.comments} ${className || ''}`} onClick={onClick}>
       <div>
         <div><CommentIcon /></div>
-        <div>{counter}</div>
+        <div>{count}</div>
       </div>
       <div className={styles.tooltip} role="tooltip">Comment</div>
     </button>
