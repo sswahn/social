6d75a7c06ffaddf0e90516f94f530e7a41eb0887
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-10 14:41:34 -0500

    Update LikeButton.js

--- src/core/LikeButton/LikeButton.js
+++ src/core/LikeButton/LikeButton.js
@@ -11,7 +11,9 @@ const LikeButton = ({ className, icon, count, onClick }) => {
 
   const handleClick = () => {
     const isLiked = !liked
+    const newCount = isLiked ? counter + 1 : counter - 1
     setLiked(isLiked)
+    setCounter(newCount)
     onClick({ liked: isLiked, count: newCount })
   }
 
