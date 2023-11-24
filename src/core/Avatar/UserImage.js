e8e71998a1bc7f2a6fc3357fd2ef6c09f1337e41
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-24 12:58:34 -0500

    Delete src/core/Avatar/UserImage.js

--- src/core/Avatar/UserImage.js
+++
@@ -1,7 +0,0 @@
-import styles from './styles.module.css'
-const UserImage = size => {
-  return (
-    <img src={image} alt={`${username}'s avatar`} width={size || '40px'} height={size || '40px'} />
-  )
-}
-export default UserImage
