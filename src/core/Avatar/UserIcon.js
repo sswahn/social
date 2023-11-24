68185fcf3924c0f9f1e8e8b2eb8f353edf129596
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-24 13:16:52 -0500

    Update UserIcon.js

--- src/core/Avatar/UserIcon.js
+++ src/core/Avatar/UserIcon.js
@@ -1,4 +1,4 @@
-const UserIcon = size => {
+const UserIcon = ({ size }) => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" height={size || '40px'} viewBox="0 0 512 512" role="img" aria-label="User Icon">
       {/* Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. */}
