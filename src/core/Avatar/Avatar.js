5f79fa1a8980dd2478138a452314f3d06ea50eb4
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-24 03:26:26 -0500

    Update Avatar.js

--- src/core/Avatar/Avatar.js
+++ src/core/Avatar/Avatar.js
@@ -2,12 +2,12 @@ import { useState } from 'react'
 import UserIcon from './UserIcon.js'
 import styles from './styles.module.css'
 
-const Avatar = ({ className, image, username, onClick }) => {
+const Avatar = ({ className, image, username, onClick, size }) => {
   return (
     <div className={`${styles.avatar} ${className || ''}`} onClick={onClick} aria-label={`${username}'s avatar`}>
       {image 
         ? <img src={image} alt={`${username}'s avatar`} />
-        : <UserIcon />
+        : <UserIcon size={size} />
       }
     </div>
   )
